a5f9e2d92315b1f3d5429d27dd546437dc21e3ce
BrandonOng07 <brandonong07@gmail.com>
2025-11-17 23:49:33 -0800

    Page 1-16

---
+++ Notes & Exercises/CH1 - Python Basics.py
@@ -0,0 +1,27 @@
+# Variable Assignment:
+pv = 100
+# pv has a value of 100
+
+# Calculate value of this money if annual rate is 1.5%, 3yrs)
+pv*(1+0.015)**3
+
+# Python Loops & Conditionals
+
+# Simple Loop Example:
+for i in range(5):
+    print("Iteration number:", i)
+
+# enumerate() function generates pair of indices starting from 0 and its corresponding value
+x=["a", "b", "z"]
+for i, value in enumerate (x):
+    print(i, value)
+
+# Conditional Example:
+age = 15
+if age < 18:
+    print("Minor")
+else:
+    print("Adult")
+
+# Data input
+# Example: Yahoo Finance pricing IBM
